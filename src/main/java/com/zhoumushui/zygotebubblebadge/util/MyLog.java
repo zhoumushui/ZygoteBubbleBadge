ef96c353e8bbd721e4689c56e2344661c5f3b772
zhoumushui <zhoumushui@gmail.com>
2017-04-15 16:11:16 +0800

    [ZMS]Add util.

---
+++ src/main/java/com/zhoumushui/zygotebubblebadge/util/MyLog.java
@@ -0,0 +1,62 @@
+package com.zhoumushui.zygotebubblebadge.util;
+
+import android.util.Log;
+
+public class MyLog {
+
+    private static boolean isDebug = true;
+    private static final String TAG_DEFAULT = "ZMS";
+
+    private static String MODULE = "[ZygoteBubbleBadge]";
+
+    public static void e(String log) {
+        if (isDebug)
+            Log.e(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void e(String tag, String log) {
+        if (isDebug)
+            Log.e(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void v(String log) {
+        if (isDebug)
+            Log.v(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void v(String tag, String log) {
+        if (isDebug)
+            Log.v(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void d(String log) {
+        if (isDebug)
+            Log.d(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void d(String tag, String log) {
+        if (isDebug)
+            Log.d(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void i(String log) {
+        if (isDebug)
+            Log.i(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void i(String tag, String log) {
+        if (isDebug)
+            Log.i(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void w(String log) {
+        if (isDebug)
+            Log.w(TAG_DEFAULT, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+    public static void w(String tag, String log) {
+        if (isDebug)
+            Log.w(tag, DateUtil.getTimeStr("HH:mm:ss") + MODULE + log);
+    }
+
+}
